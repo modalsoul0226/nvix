401b1092b69e6c134d61e839587f00bbd834548f
modalsoul0226 <modalsoul0226@gmail.com>
2025-05-14 16:16:31 -0400

    feat: add formatter for nix

--- modules/lang/nix.nix
+++ modules/lang/nix.nix
@@ -1,3 +1,8 @@
+{ lib, pkgs, ... }:
 {
   plugins.lsp.servers.nixd.enable = true;
+  plugins.conform-nvim.settings = {
+      formatters_by_ft.nix = [ "nixfmt" ];
+      formatters.nixfmt.command = lib.getExe pkgs.nixfmt-rfc-style;
+    };
 }
