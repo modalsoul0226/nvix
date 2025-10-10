82e7324630025e642b930b03c52069f76147c3cd
modalsoul0226 <modalsoul0226@gmail.com>
2025-10-10 11:16:13 -0400

    configure codeium

--- modules/cmp/default.nix
+++ modules/cmp/default.nix
@@ -36,17 +36,16 @@
           { name = "nvim_lsp_signature_help"; }
           { name = "path"; }
           { name = "buffer"; }
+          { name = "codeium"; }
         ];
       };
     };
   };
 
-  imports = with builtins; with lib;
-    map (fn: ./${fn})
-      (filter
-        (fn: (
-          fn != "default.nix"
-          && !hasSuffix ".md" "${fn}"
-        ))
-        (attrNames (readDir ./.)));
+  imports =
+    with builtins;
+    with lib;
+    map (fn: ./${fn}) (
+      filter (fn: (fn != "default.nix" && !hasSuffix ".md" "${fn}")) (attrNames (readDir ./.))
+    );
 }
--- modules/copilot/default.nix
+++ modules/copilot/default.nix
@@ -2,21 +2,22 @@
 {
   plugins.windsurf-nvim = {
     enable = true;
-    # settings = {
-    #   filetypes.markdown = true;
-    #   suggestion = {
-    #     enabled = true;
-    #     auto_trigger = true;
-    #   };
-    # };
+    settings = {
+      virtual_text = {
+        enabled = true;
+      };
+      # filetypes.markdown = true;
+      # suggestion = {
+      #   enabled = true;
+      #   auto_trigger = true;
+      # };
+    };
   };
 
-  imports = with builtins; with lib;
-    map (fn: ./${fn})
-      (filter
-        (fn: (
-          fn != "default.nix"
-          && !hasSuffix ".md" "${fn}"
-        ))
-        (attrNames (readDir ./.)));
+  imports =
+    with builtins;
+    with lib;
+    map (fn: ./${fn}) (
+      filter (fn: (fn != "default.nix" && !hasSuffix ".md" "${fn}")) (attrNames (readDir ./.))
+    );
 }
